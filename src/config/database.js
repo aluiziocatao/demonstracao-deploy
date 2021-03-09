a320021bfafcd2384d3a9a2c8e5e912ceb67e03c
Aluízio <aluizio2210@gmail.com>
2021-03-09 18:49:38 -0300

    Mudanças nas configurações do Heroku

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 module.exports = {
-  username: 'b20920d128440d',
-  password: 'c64b141e',
-  database: 'heroku_60ff8d9a43205ce',
+  username: 'b20aa7ef6a93c2',
+  password: '3e6427ac',
+  database: 'heroku_ebe33096ec54929',
   host: 'us-cdbr-east-03.cleardb.com',
   dialect: 'mysql'
 }
